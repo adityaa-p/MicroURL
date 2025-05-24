fe2268e26e1db007d05a880f5f6a57cd90186d6b
Aditya Palpattuwar <aditya.palpattuwar@everest.engineering>
2025-05-24 23:13:27 +0530

    Add new post endpoint to add new long url

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    url: str
+    expiration_date: str
+    custom_alias: str | None = None
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.post("/urls")
+async def post(item : Item):
+    return item
